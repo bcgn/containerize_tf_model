cf4f8cd46aa78e2e4f9b8ab6fda443573a3df01e
bcgn <robert.titz@gmail.com>
2022-05-31 12:18:01 +0200

    Format code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root() -> dict[str, str]:
     return {"message:": "Hello World"}
--- tests/test_containerize_tf_model.py
+++ tests/test_containerize_tf_model.py
@@ -2,4 +2,4 @@ from containerize_tf_model import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
